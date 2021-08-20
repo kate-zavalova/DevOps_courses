2193b1a6949ef9752a752af20ff40f7af171c5fa
kate-zavalova <87708438+kate-zavalova@users.noreply.github.com>
2021-08-20 18:45:17 +0300

    Add files via upload

---
+++ 4.Linux/random_cube.sh
@@ -0,0 +1,10 @@
+for ((i = 0; i < 60; i++)); do      
+    moves[i]=$[$RANDOM % 6 + 1]
+done
+
+pips=(ones twos threes fours fives sixes)
+count=( $((IFS=$'\n';sort <<< "${moves[*]}") | uniq -c |  awk '{print $1}' ))
+
+for ((i=0; i< 6; i++)) do 
+  printf "%s\t%s\n" "${pips[$i]}" "${count[$i]}"; 
+done
