199e6f64db349aa3af860588da169bbbd4690a33
kate-zavalova <87708438+kate-zavalova@users.noreply.github.com>
2021-08-21 01:51:25 +0300

    Delete yandex2.sh

--- 4.Linux/yandex2.sh
+++
@@ -1,5 +0,0 @@
-curl https://www.google.com/accounts/ClientLogin \
---data-urlencode Email=okvoda@gmail.com --data-urlencode Passwd=311rjkjubz \
--d accountType=GOOGLE \
--d source=Google-cURL-Example \
--d service=lh2
